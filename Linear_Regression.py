a32f3fe868c7a894acc21e633e3020a2acb930c3
Annish jr <163139205+Darkcoder-Annishjr@users.noreply.github.com>
2024-04-22 19:44:42 +0530

    Create Linear_Regression.py

---
+++ Linear_Regression.py
@@ -0,0 +1,19 @@
+#IMPORTING ALL THE PYTHON LIBRARIES THAT WE GONNA USE FOR LINEAR REGRESSION
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split  #importing this to train and test the accuracy 
+from sklearn.linear_model import LinearRegression
+
+#IMPORTING THE DATA 
+df = pd.read_csv("")
+
+#
+
+
+
+
+
+
+
