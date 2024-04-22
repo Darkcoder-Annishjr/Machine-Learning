66f85ec1017933d2ce0d1fa3af7aaf7f736a86f2
Annish jr <163139205+Darkcoder-Annishjr@users.noreply.github.com>
2024-04-22 20:23:27 +0530

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -4,12 +4,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split  #importing this to train and test the accuracy 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression as lr
 
-#IMPORTING THE DATA 
+# IMPORTING THE DATA 
 df = pd.read_csv("")
 
-#
+# CLEANING THE DATA (Duplicates, Null Values) 
+df.isna.sum()
+
+df.duplicated()
+
+df.drop_duplicates()
+
+# VIEW INFO OF THE DATA (Shape, Data_Type, Descriptive Statistics)
+df.shape
+
+df.info()
+
+df.describe()
+
+# TO VISUALIZE THE DATA
+plt.scatter()
+plt.xlabel("")
+plt.ylabel("")
+
+# FITTING THE LINEAR REGRESSION
+lr = LinearRegression()
+
+lr.fit()
+
 
 
 
